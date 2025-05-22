b14bd2409c8fb593a04dd332b027516904d45f63
clementwa0 <clementwa01@gmail.com>
2025-05-22 16:43:12 +0300

    Build Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { sidebarItems } from '~/constants'
 import { Link, NavLink } from 'react-router'
+import { cn } from '~/lib/utils'
 
 const Navbar = () => {
+    const user = {
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        imageurl: '/public/assets/images/david.webp'
+    }
   return (
     <section className="nav-items">
         <Link to='/' className="link-logo">
@@ -12,15 +18,45 @@ const Navbar = () => {
 
         <div className='container'>
             <nav>
-                {sidebarItems.map(({id, href, icon,label}) => (
-                    <NavLink to={href} key={id}>
-                        {({isActive}:{isActive:boolean}) => (
-                           <div>
-                            {label}
-                           </div>)}
+                {sidebarItems.map(({id, href, icon, label}) => (
+                    <NavLink
+                        to={href}
+                        key={id}>
+                            
+                            {({ isActive }: { isActive: boolean }) => (
+                           <div className={cn("group nav-item", { 'bg-primary !text-white': isActive 
+})}>
+                        <img 
+                        src={icon}
+                        alt={label}
+                        className={`group-hover:brightness-0 size-0 group-hover:invert ${isActive ? ' brightness-0 invert' : ''}`}/>
+                        {label}
+                        </div>
+)}
                     </NavLink>
                 ))}
             </nav>
+
+            <footer className='nav-footer'>
+                <img src={user?.imageurl || '/public/assets/images/david.webp'} alt={user?.name}/>
+
+                <article>
+                    <h2>{user?.name}</h2>
+                    <p>{user?.email}</p>
+                </article>
+
+                <button 
+                onClick={() => {
+                    console.log('Logout')
+                }}
+                className='cursor-pointer'>
+                    <img 
+                    src='/public/assets/icons/logout.svg'
+                    alt='logout'
+                    className='size-10'/>
+                    </button>
+                </footer>
+
         </div>
     </section>
   )
