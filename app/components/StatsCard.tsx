84e91a55239cff491a24465e1115bc5b0ebf7b52
clementwa0 <clementwa01@gmail.com>
2025-06-02 21:54:25 +0300

    feat: add reusable StatsCard component with trend calculation and dynamic up/down icon based on percentage change

--- app/components/StatsCard.tsx
+++ app/components/StatsCard.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import { calculateTrendPercentage } from "~/lib/utils"
 
-const StatsCard = () => {
+const StatsCard = ({ 
+    headerTitle,
+    total,
+    currentMonthCount,
+    lastMonthCount}:StatsCard) => {
+
+        const { trend, percentage} = calculateTrendPercentage
+        (currentMonthCount, lastMonthCount);
+
+        const isDecrement = trend === 'decrement';
   return (
-    <div>StatsCard</div>
+    <article className="stats-card">
+            <h3 className="text-base font-medium">
+                {headerTitle}
+            </h3>
+
+            <div className="content">
+                <div className="flex flex-col gap-4">
+                    <h2 className="text-4xl font-semibold">
+                        {total}
+                    </h2>
+
+                <div className="flex items-center gap-2">
+                    <figure className="flex items-center gap-2">
+                        <img 
+                        src={`/assets/icons/${isDecrement ? 'arrow-down-red.svg' : 'arrow-up-green.svg'}`}
+                        className="size-5" alt="trend icon" />
+                        <figcaption>
+                            {Math.round(percentage)}%
+                        </figcaption>
+                        </figure>
+                </div>
+                </div>
+            </div>
+    </article>
   )
 }
 
