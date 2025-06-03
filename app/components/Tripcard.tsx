91b703e59c953c50970e4b8d254446616480d7c4
clementwa0 <clementwa01@gmail.com>
2025-06-03 18:25:03 +0300

    feat(components): create TripCard component
    - Add TripCard component with responsive layout
    - Implement dynamic routing based on location path
    - Add chip list for trip tags with custom styling
    - Display trip details (name, location, price)
    - Integrate SyncFusion chip components
    - Add hover effects and card styling

--- app/components/Tripcard.tsx
+++ app/components/Tripcard.tsx
@@ -1,11 +1,42 @@
-import { Link } from "react-router"
-
+import { Link, useLocation } from "react-router"
+import { cn, getFirstWord } from "~/lib/utils";
+import pkg from '@syncfusion/ej2-react-buttons';
+const {ChipDirective, ChipListComponent, ChipsDirective} = pkg;
 const Tripcard = ({id,name,location,imageUrl,tags,price}:
   TripCardProps ) => {
+    const path = useLocation();
+
   return (
-    <Link to={`/trips/${id}`} className="trip-card bg-white shadow-md hover:shadow-lg transition-shadow duration-300 rounded-lg p-4">
-      <h2 className="text-lg font-semibold mb-2">{name}</h2> 
-    <img src={imageUrl} alt={name}  />
+    <Link to={path.pathname==='/' || path.pathname.startsWith('/travel')? `/travel/${id}`: `/trips/${id}`} 
+    className="trip-card bg-white rounded-lg p-4">
+    <img src={imageUrl} alt="Trips"  />
+    <article>
+      <h2>{name}</h2>
+      <figure>
+        <img 
+        src="/assets/icons/location-mark.svg"
+        alt="location" className="size-4"/>
+        <figcaption>{location}</figcaption>
+      </figure>
+    </article>
+
+
+      <div className="mt-5 pl-[18px] pr-3.5 pb-5">
+                <ChipListComponent id="travel-chip">
+                    <ChipsDirective>
+                        {tags?.map((tag, index) => (
+                            <ChipDirective
+                                key={index}
+                                text={getFirstWord(tag)}
+                                cssClass={cn(index===1
+                                ? '!bg-pink-50 !text-pink-500'
+                                : '!bg-success-50 !text-success-700')}
+                            />
+                        ))}
+                    </ChipsDirective>
+                </ChipListComponent> 
+            </div>
+        <article className="tripCard-pill">{price}</article>
     </Link>
   )
 }
