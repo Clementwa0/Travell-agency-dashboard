eae0d25b15e34244b87fbef1571cebce7994a7f8
clementwa0 <clementwa01@gmail.com>
2025-06-10 14:44:41 +0300

    Implemented Sign in Functionality using Google Auth

--- app/routes/admin/UsersPage.tsx
+++ app/routes/admin/UsersPage.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Header } from '~/components';
+
 import pkg from '@syncfusion/ej2-react-grids';
 const {GridComponent, ColumnDirective, ColumnsDirective, Page, Sort, Filter, Inject} = pkg;
-
 import { Users } from "~/constants";
 
 const UsersPage = () => {
@@ -14,11 +14,7 @@ const UsersPage = () => {
       />
       <GridComponent 
         dataSource={Users} 
-        allowPaging={true}
-        allowSorting={true}
-        allowFiltering={true}
-        pageSettings={{ pageSize: 10 }}
-        className="mt-4"
+       
       >
         <ColumnsDirective>
           <ColumnDirective 
@@ -27,27 +23,8 @@ const UsersPage = () => {
             width='150' 
             textAlign='Left'
           />
-          <ColumnDirective 
-            field='email' 
-            headerText='Email' 
-            width='200'
-          />
-          <ColumnDirective 
-            field='role' 
-            headerText='Role' 
-            width='100'
-          />
-          <ColumnDirective 
-            field='status' 
-            headerText='Status' 
-            width='100'
-          />
-          <ColumnDirective 
-            field='lastLogin' 
-            headerText='Last Login' 
-            width='150' 
-            format='yMd'
-          />
+        
+          
         </ColumnsDirective>
         <Inject services={[Page, Sort, Filter]}/>
       </GridComponent>
--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,12 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { sentryReactRouter, type SentryReactRouterBuildOptions } from "@sentry/react-router";
 
-const sentryConfig: SentryReactRouterBuildOptions = {
-  org: "klemoh",
-  project: "travel",
-  // An auth token is required for uploading source maps.
-  authToken: "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"
-  // ...
-};
+export default defineConfig({
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 
-export default defineConfig(config => {
-  return {
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), 
-    reactRouter(), sentryReactRouter(sentryConfig, config)],
-
-
-  ssr: {
+  ssr:{
     noExternal: [
       "@syncfusion"]
-
-  }
   }
 });
